99c284c38273800a7b5adc9b5aca36cdf10177d6
Patrik <pirtanp@duck.com>
2022-12-18 23:43:30 +0100

    new website built from scratch is done. Now doing responsiveness for whole website

---
+++ index.js
@@ -0,0 +1,14 @@
+// Button popup
+
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+
+
